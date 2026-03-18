a4fbbac42b9a09729d28c34f28c0185bffd59549
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-18 03:37:29 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use Symfony\Component\Routing\Route as RoutingRoute;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('register', [UserController::class, 'register']);
+Route::get('register', [UserController::class, 'showRegister']);
+Route::get('login', [UserController::class, 'login']);
+Route::get('login', [UserController::class, 'showLogin']);
