3e0a4b5db9ff48884efa65bc655b397051a13d5e
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-18 03:44:56 +0100

    controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function create(){
+        $users = DB::table('users')->first();
+        return view('register');
+    }
+
+    public function store(Request $request){
+        DB::table('users')->insert([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'password'=>$request->password
+        ]);
+        return redirect('/page');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use Symfony\Component\Routing\Route as RoutingRoute;
 
-Route::get('register', [UserController::class, 'register']);
-Route::get('register', [UserController::class, 'showRegister']);
+Route::get('register', [UserController::class, 'create']);
+Route::get('register', [UserController::class, 'store']);
 Route::get('login', [UserController::class, 'login']);
 Route::get('login', [UserController::class, 'showLogin']);
