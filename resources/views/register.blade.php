9ece6339f85488c048a057fcb2800c050022a878
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-19 04:31:25 +0100

    register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>sign up</title>
 </head>
 <body>
-    
+    <h1>create acount</h1><br>
+    <form action="/register">
+        @csrf
+        <input type="text" name="name" placeholder="username" required><br>
+        <input type="email" name="email" placeholder="email" required><br>
+        <input type="password" name="password" placeholder="password" required><br>
+        <button type="submit">save</button><br>
+        <a href="/login">Login</a>
+
+
+
+    </form>
 </body>
 </html>
