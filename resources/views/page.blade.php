b890f8cb6311eb4f70e7c7410eb5df1079d56e44
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-19 04:20:27 +0100

    page

---
+++ resources/views/page.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>task manager</title>
+</head>
+
+<body>
+    <h2>Welcome</h2>
+
+    @if(session('user'))
+    <p>Hello, {{ session('user')->name }}</p>
+    @endif
+
+    <form method="POST" action="/logout">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+
+</body>
+
+</html>
