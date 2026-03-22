394a41965eab82741c5086d15e00abd4311a7c7f
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-22 05:26:10 +0100

    intro

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -168,7 +168,7 @@
                 <ul class="navbar-nav ms-auto">
 
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">
+                        <a class="nav-link active" href="/intro">
                             <i class="fas fa-info-circle"></i> About Us
                         </a>
                     </li>
