e106a32d7125180950f2f288815fed255269f6a2
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-24 03:19:17 +0100

    D_controller

--- app/Http/Controllers/DboardController.php
+++ app/Http/Controllers/DboardController.php
@@ -4,8 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DboardController extends Controller
 {
-    
+    public function dashboard(){
+        $dboard = DB::table('dashboard')->get();
+        return view('dashboard');
+
+    }
+
+    public function create(){
+        return view('create');
+    }
+
+    public function store(Request $request){
+        DB::table('dashboard')->insert([
+            
+        ]);
+    }
 }
