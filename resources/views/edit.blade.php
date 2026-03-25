69f05cd7804764b108ada32989b6d3b1964cbc13
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-25 23:31:34 +0100

    edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,6 +6,16 @@
     <title>edit</title>
 </head>
 <body>
-    
+    <form action="/update/{{$dboards->id}}">
+        @csrf
+        <input type="text" name="title" value="{{$dboards->title}}" required><br>
+        <input type="text" name="description" value="{{$dboards->description}}" required><br>
+        <input type="date" name="date" value="{{$dboards->date"}} required> <br>
+        <select name="status" required>
+            <option value="pending">Pending</option>
+            <option value="done">Done</option>
+        </select><br>
+        <button type="submit">update</button>
+    </form>
 </body>
 </html>
