7b2703eac403169971735a2947261c5e94da3da0
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-25 23:20:25 +0100

    create

---
+++ resources/views/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>create mission</title>
+</head>
+
+<body>
+    <form action="/store" method="post">
+        @csrf
+        <input type="text" name="title" placeholder="title" required><br>
+        <input type="text" name="description" placeholder="description" required><br>
+        <input type="date" name="date" placeholder="date" required><br>
+        <select name="status" required>
+            <option value="pending">Pending</option>
+            <option value="done">Done</option>
+        </select><br>
+        <button type="submit">save</button>
+
+    </form>
+</body>
+
+</html>
