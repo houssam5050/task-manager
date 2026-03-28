5c0b6275304f5eef344da41da6964de4c28bffae
houssam5050 <hossamhamdan4545@gmail.com>
2026-03-28 23:14:58 +0100

    migration

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('dashboard', function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->text('title');
             $table->text('description')->nullable();
